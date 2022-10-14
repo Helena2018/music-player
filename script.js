63c7abb3190b391a90723e92591f9afd19fcc73d
Helena2018 <helena2020liu@gmail.com>
2022-10-15 12:56:28 +1300

    import img, title, artist and music data

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
@@ -5,13 +8,25 @@ const nextBtn = document.getElementById('next');
 
 let isPlaying = false;
 
+const song = [
+  {
+    name: 'metric"-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Metric/Jacinto Design',
+  }
+]
+
 function playSong() {
   isPlaying = true;
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.getAttribute('title', 'Pause')
   music.play();
 }
 
 function pauseSong() {
   isPlaying = false;
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.getAttribute('title', 'Play')
   music.pause();
 }
 
